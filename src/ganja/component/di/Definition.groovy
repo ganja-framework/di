3f73a4cafb6b39311aba559434f37d6461f1f8c3
Janusz Slota <janusz.slota@nixilla.com>
2014-08-19 16:44:26 +0100

    Added definition tagging

--- src/ganja/component/di/Definition.groovy
+++ src/ganja/component/di/Definition.groovy
@@ -4,11 +4,33 @@ class Definition {
 
     String className
     def arguments
+    List<String> tags = []
 
     Definition setArguments(def args) {
 
         arguments = args
 
         this
+    }
+
+    void addTag(String tag) {
+
+        if( ! tags.contains(tag)) {
+
+            tags.add(tag)
+        }
+    }
+
+    Boolean hasTag(String tag) {
+        tags.contains(tag)
+    }
+
+    void clearTags() {
+        tags = []
+    }
+
+    void clearTag(String tag) {
+
+        tags.remove(tag)
     }
 }
--- test/ganja/component/di/DefinitionSpec.groovy
+++ test/ganja/component/di/DefinitionSpec.groovy
@@ -13,7 +13,7 @@ class DefinitionSpec extends Specification {
         definition instanceof Definition
     }
 
-    void "it can set contructor arguments"() {
+    void "it can set constructor arguments"() {
 
         given:
         def definition = new Definition()
@@ -23,5 +23,39 @@ class DefinitionSpec extends Specification {
 
         then:
         definition instanceof Definition
+    }
+
+    void "it can be tagged"() {
+
+
+        given:
+        def definition = new Definition()
+
+        when:
+        definition.addTag('listener')
+
+        then:
+        definition.hasTag('listener')
+
+        when:
+        definition.clearTags()
+
+        then:
+        definition.getTags() == []
+
+        when:
+        definition.setTags([ 'listener', 'my.tag' ])
+
+        then:
+        definition.getTags() == [ 'listener', 'my.tag' ]
+        definition.hasTag('listener')
+        definition.hasTag('my.tag')
+
+        when:
+        definition.clearTag('my.tag')
+
+        then:
+        definition.hasTag('listener')
+        ! definition.hasTag('my.tag')
     }
 }
